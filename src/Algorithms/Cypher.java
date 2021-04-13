a97e16f12f6781138d37b92c26e5793f2eb23558
zs-richy <zs.ricsi@gmail.com>
2021-04-13 16:17:11 +0200

    Now able to encrypt strings longer than 'n' (public key component) by dividing to smaller blocks

--- src/Algorithms/Cypher.java
+++ src/Algorithms/Cypher.java
@@ -1,6 +1,8 @@
 package Algorithms;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static Algorithms.Algorithms.FME;
 
@@ -8,6 +10,7 @@ public class Cypher {
 
     public static BigInteger Encrypt(Key kulcs, BigInteger message) {
         BigInteger[] publicKey = kulcs.getPublicKey();
+
         return (FME(publicKey[0], message, publicKey[1]));
     }
 
@@ -26,10 +29,42 @@ public class Cypher {
 
     public static BigInteger Encrypt(Key kulcs, String message) {
         byte[] allBytes = message.getBytes();
+        BigInteger[] publicKey = kulcs.getPublicKey();
         BigInteger mToEncrypt = new BigInteger(allBytes);
+
+        if (publicKey[0].compareTo(mToEncrypt) == -1) {
+            throw new IllegalArgumentException("Message is longer than public key! Call block encrypt function!");
+        }
 
         return Encrypt(kulcs, mToEncrypt);
 
     }
+
+    public static ArrayList<BigInteger> blockEncrypt(Key kulcs, String message) {
+        ArrayList<BigInteger> result = new ArrayList<BigInteger>();
+        char[] msgChars = message.toCharArray();
+
+        int i = 1;
+        int j = 0;
+        byte[] tempBytes;
+        BigInteger tempRes;
+        while (i <= msgChars.length) {
+            tempBytes = new String(Arrays.copyOfRange(msgChars,j,i)).getBytes();
+            tempRes = new BigInteger(tempBytes);
+            if (tempRes.compareTo(kulcs.getPublicKey()[0]) == 0 || tempRes.compareTo(kulcs.getPublicKey()[0]) == 1) {
+                tempBytes = new String(Arrays.copyOfRange(msgChars,j,i-1)).getBytes();
+                tempRes = new BigInteger(tempBytes);
+                result.add(Encrypt(kulcs,tempRes));
+                j = i-1;
+                i--;
+            }
+            if (i == msgChars.length) {
+                result.add(Encrypt(kulcs,tempRes));
+            }
+            i++;
+        }
+        return result;
+    }
+
 
 }
